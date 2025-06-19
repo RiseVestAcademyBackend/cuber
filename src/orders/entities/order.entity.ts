78ed3723a6c29a20b665ad83b04636a451da607b
Oluwafemi <ojurifemi132@gmal.com>
2025-06-19 18:15:50 +0100

    chore: order entity

--- src/orders/entities/order.entity.ts
+++ src/orders/entities/order.entity.ts
@@ -1,25 +1,57 @@
-import {Entity, Column, ManyToMany, OneToMany, RelationId  } from "typeorm";
-import { Field, ObjectType, InputType } from "@nestjs/graphql";
-import { Restaurant } from "src/restaurants/entities/restaurant.entity";
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  OneToOne,
+  JoinColumn,
+  UpdateDateColumn,
+  CreateDateColumn,
+  OneToMany,
+} from 'typeorm';
+import { Restaurant } from 'src/restaurants/entities/restaurant.entity';
+import { Customer } from 'src/customers/entities/customer.entity';
+import { Driver } from 'src/drivers/entities/driver.entity';
+import { orderStatus } from '../enums/order-status.enum';
+import { Food } from 'src/food/entities/food.entity';
 
-@ObjectType()
 @Entity()
 export class Order {
-        @Column()
-        @Field(() => String)
-        customer: string;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-        @RelationId((order: Order) => String)
-        customerId: number;
-        
-        driver: string;
-        
-        driverId: number;
-        restaurant: Restaurant;
+  @Column({
+    type: 'number',
+    nullable: false,
+  })
+  total_price: number;
 
-        @Column()
-        @Field(() => String)
-        total_price: number;
-        
-        status: string;
+  @Column({
+    type: 'enum',
+    enum: orderStatus,
+    nullable: false,
+    default: orderStatus.PENDING,
+  })
+  status: orderStatus;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @OneToOne(() => Customer)
+  @JoinColumn()
+  customer: Customer;
+
+  @OneToOne(() => Driver)
+  @JoinColumn()
+  driver: Driver;
+
+  @OneToOne(() => Restaurant)
+  @JoinColumn()
+  restaurant: Restaurant;
+
+  // Uncomment when food entity is done
+  // @OneToMany(()=> Food, (food) => food.id)
+  food?: Food[];
 }
