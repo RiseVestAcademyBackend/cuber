9a1bd35c9b82b666517dfd101e23c0da4a7d6965
Oluwafemi <ojurifemi132@gmal.com>
2025-06-22 23:10:37 +0100

    chore: Setup a filter to reformat response when exception message(s) thrown by class validator come as an array.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { GlobalHttpExceptionFilter } from './filters/global-http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalFilters(new GlobalHttpExceptionFilter());
 
   app.useGlobalPipes(
     new ValidationPipe({
